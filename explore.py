59227f96f0bc367d61987e05e85f8f49f1dcfa83
Nodar Gogoberidze <gnodar01@gmail.com>
2026-05-15 10:44:50 -0400

    Edit text to be more slide-appropriate

--- explore.py
+++ explore.py
@@ -1,7 +1,7 @@
 import marimo
 
 __generated_with = "0.23.6"
-app = marimo.App(width="medium", layout_file="layouts/explore.grid.json")
+app = marimo.App(width="medium", layout_file="layouts/explore.slides.json")
 
 
 @app.cell(hide_code=True)
@@ -49,39 +49,27 @@ def _():
 def _(mo):
     mo.md(r"""
     ## SWE-Bench
-
-    ### Paper highlights
-
-    [SWE-bench: can language models resolve real-world GitHub Issues](https://openreview.net/pdf?id=VTF8yNQM66) (Jimenez, et. al.; ICLR; 2024)
-
-    The [original SWE-bench](https://www.swebench.com/original.html), released in late 2023, tests AI systems' ability to solve GitHub Issues. It is a collection of 2,294 task instances collected by crawling PRs and Issues from 12 popular Python repositories (filtered down from 90,000 by excluding PRs not associated with issues, not associated with a fail-to-pass test outcome before and after the PR is applied, or having a runtime/installation error). A criteria used is that each PR is associated with an issue and modified >= 1 testing-related file. Each instance is put into a Docker image with the commit associated with the PR. Pre-PR, there are failing tests, and post-PR all tests pass.
-
-    > In the real world... Fixing a bug might involve navigating a large repository, understanding the interplay between functions in different files, or spotting a small error in convoluted code. Inspired by this, we introduce SWE-bench, a benchmark that evaluates LMs in a realistic software engineering setting.
     """)
-    return
-
-
-@app.cell
-def _(mo):
-    mo.image('public/swe-bench_overview.png')
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    The AI system is given the issue text (both bug reports and feature requests), and must modify the codebase in order to resolve the described issues. The fail-to-pass tests are used to evaluate the modifications and are not given as input. An extension also uses pass-to-pass tests.
+    ### Paper highlights
 
-    Resolving issues in SWE-bench frequently requires understanding and coordinating changes across multiple functions, classes, and even files simultaneously, calling for models to interact with execution environments, process extremely long contexts and perform complex reasoning that goes far beyond traditional code generation tasks.
+    [SWE-bench: can language models resolve real-world GitHub Issues](https://openreview.net/pdf?id=VTF8yNQM66) (Jimenez, et. al.; ICLR; 2024)
 
-    At the time of publishing, the best model on this benchmark was Claude 2, which could solve 1.96% of issues.
+    > In the real world... Fixing a bug might involve navigating a large repository, understanding the interplay between functions in different files, or spotting a small error in convoluted code. Inspired by this, we introduce SWE-bench, a benchmark that evaluates LMs in a realistic software engineering setting.
 
-    Compared to other benchmarks (esp. at the time):
-    * real-world SWE tasks
-    * easily extensible corpus of tasks
-    * diverse long inputs - issues are typically long and detailed
-    * robust evaluation - at least one fail-to-pass test, with 40% having two or more
-    * wide scope for possible solutions
+    The [original SWE-bench](https://www.swebench.com/original.html), released in late 2023, tests AI systems' ability to solve GitHub Issues.
+
+    * 2,294 task instances
+    * scraped PRs and Issues from 12 popular Python repositories
+    * filtered down from 90,000
+    * PR must have issue
+    * must have fail-to-pass test outcome before & after PR applied
+    * no runtime/installation error
     """)
     return
 
@@ -95,13 +83,54 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
+    AI system given issue text (both bug reports and feature requests), and must modify the codebase in order to resolve the described issues.
+    Fail-to-pass tests are used to evaluate the modifications and are not given as input.
+    An extension also uses pass-to-pass tests.
+
+    > Resolving issues in SWE-bench frequently requires understanding and coordinating changes across multiple functions, classes, and even files simultaneously, calling for models to interact with execution environments, process extremely long contexts and perform complex reasoning that goes far beyond traditional code generation tasks.
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    mo.image('public/swe-bench_overview.png')
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    At the time of publishing, the best model on this benchmark was Claude 2, which could solve 1.96% of issues.
+
+    Compared to other benchmarks (esp. at the time):
+    * real-world SWE tasks
+    * easily extensible corpus of tasks
+    * diverse long inputs - issues are typically long and detailed
+    * robust evaluation - at least one fail-to-pass test, with 40% having two or more
+    * wide scope for possible solutions
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
     #### Retrieval-based approach
 
-    SWE-bench instances provide an issue description and a codebase as input to the model. While issue descriptions are usually short, codebases consist of many more tokens (438K lines on average) than can typically be fit into an LMs context window. To rank and select appropriate files to insert into the context, a retrieval system was used with two context settings:
-    1) Sparse retrieval. BM25 (Robertson et L., 2009) retrieval: a ranking function used in search systems.
-    2) "Oracle retrieval". Files edited by the reference patch are retrieved. Less realistic since an engineer wouldn't have this information given to them, and limiting since there are other untouched files that are still relevant to understanding the full context of the issue.
+    SWE-bench instances provided an issue description and codebase as input to the model.
+
+    Codebases consist of many tokens (438K lines on average). More than can fit into an LMs context window.
+
+    To rank & select appropriate files for insertion into context, a retrieval system was used with two context settings:
+
+    1) Sparse retrieval / BM25 (Robertson et L., 2009) retrieval - a ranking function used in search systems.
 
     In 40% of instances, BM25 retrieves a superset of the oracle files. In almost half of instances, it retrieves none of the oracle files.
+
+    2) "Oracle retrieval" - Files edited by the reference patch are retrieved.
+
+    Oracle Retrieval is less realistic since an engineer wouldn't have this information given to them, and limiting since there are other untouched files that are still relevant to understanding the full context of the issue.
     """)
     return
 
@@ -139,13 +168,13 @@ def _(mo):
     mo.md(r"""
     #### SWE-bench Verified
 
-    [SWE-bench verified](https://www.swebench.com/verified.html) is a human-filtered subset of 500 instances from SWE-bench in [collab with OpenAI](https://openai.com/index/introducing-swe-bench-verified/). Human annotators reviewed each instance to ensure the problem descriptions are clear, the test patches correct, and the tasks are solvable given the available information.
+    [SWE-bench verified](https://www.swebench.com/verified.html) is a human-filtered subset of 500 instances from SWE-bench in [collab with OpenAI](https://openai.com/index/introducing-swe-bench-verified/).
 
-    The instances are also categorized into difficulty categories from 'easy' (196 <15-minute fix tasks) to 'hard' (45 >1-hour tasks).
+    * Human annotators reviewed each instance to ensure the problem descriptions are clear, the test patches correct, and the tasks are solvable given the available information.
+    * The instances are also categorized into difficulty categories.
+    * The verified leaderboard features results from a wide variety of AI coding systems, from simple LM agent loops to RAG systems to multi-rollout and review type systems.
 
-    The verified leaderboard features results from a wide variety of AI coding systems, from simple LM agent loops to RAG systems to multi-rollout and review type systems.
-
-    For apples-to-apples comparison of LMs, all LMs use [mini-swe-agent](https://github.com/SWE-agent/mini-swe-agent) in a minimal BASH environment. No tools, no special scaffold structure. Only a simple [ReAct](https://arxiv.org/abs/2210.03629) agent loop.
+    For apples-to-apples comparison of LMs, all LMs use [mini-swe-agent](https://github.com/SWE-agent/mini-swe-agent) in a minimal BASH environment. Only a simple [ReAct](https://arxiv.org/abs/2210.03629) agent loop is used. No tools or special scaffold are provided.
     """)
     return
 
@@ -325,15 +354,28 @@ def _(mo):
     mo.md(r"""
     ## Terminal-bench
 
-    ### Paper highlights
-
     Formerly called cli-bench, with [initial commit](https://github.com/harbor-framework/terminal-bench/commits/main/?after=1a6ffa9674b571da0ed040c470cb40c4d85f9b9b+890) on Jan 17, 2025, then T-bench, [then](https://github.com/harbor-framework/terminal-bench/commit/88387bef42a3f56b7241fdd30ba3b56daf2f8c87) terminus, then [a day later](https://github.com/harbor-framework/terminal-bench/commit/b099cd498965b729bb0e4a24673919c7057182c0) on May 19, 2025 to Terminal-bench.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Paper highlights
 
     [Terminal-bench: benchmarking agents on hard, realistic tasks in command line interfaces](https://arxiv.org/pdf/2601.11868) (Merrill et al., 2026; arXiv)
 
     > Current benchmarks either do not measure real-world tasks, or are not sufficiently difficult to meaningfully measure frontier models. To this end, we present Terminal-Bench 2.0: a carefully curated hard benchmark composed of 89 tasks in computer terminal environments inspired by problems from real workflows. Each task features a unique environment, human written solution, and comprehensive tests for verification.
 
-    A Terminal-Bench task consists of an instruction, a Docker image, a set of tests, an example solution, and a time limit. The tests do not test the agent’s commands or console output, as Terminal-bench is an *outcome-driven* framework where agents are free to accomplish the task using a variety of approaches.
+    A Terminal-Bench task consists of:
+    * an instruction
+    * a Docker image
+    * a set of tests
+    * an example solution
+    * a time limit.
+
+    The tests do not test the agent’s commands or console output, as Terminal-bench is an *outcome-driven* framework where agents are free to accomplish the task using a variety of approaches.
     """)
     return
 
@@ -349,11 +391,10 @@ def _(mo):
     mo.md(r"""
     #### Tasks
 
-    Tasks are interactive. Within the bounds of the Docker environment the agent must explore and manipulate the environment by calling tools.
+    * interactive - within the bounds of the Docker environment the agent must explore and manipulate the environment by calling tools
+    * crowd-sourced through open-source contributions
 
-    Terminal-bench 2.0 contains a diverse range of tasks which are crowd-sourced through open-source contributions. 93 contributors created 229 tasks, of which 89 were selected for inclusion based on the difficulty and quality assessments made by three experienced human reviewers.
-
-    Each task has an oracle solution. Task authors proved a guess on how long they think a junior SWE with little familiarity with the topic would take to complete the task, called the "junior time estimate", and how long a domain expert would require, called the "expert time estimate". Authors also reported a high-level category for each task (SWE, Sys Admin, Data Science, Security, Scientific Computing, File Operations, Debugging, Data Processing, Model Training, Mathematics, Machine Learning, Games, Video Processing, Data Querying, Optimization, Personal Assistant).
+    93 contributors created 229 tasks, of which 89 were selected for inclusion based on the difficulty and quality assessments made by three experienced human reviewers
     """)
     return
 
@@ -363,9 +404,11 @@ def _(mo):
     mo.md(r"""
     #### Terminus 2
 
-    Because Terminal-Bench is an interactive framework, agent and model performance are hard to decouple. Many agent scaffolds have been engineered to accommodate the tendencies of certain models, especially when the model and agent are developed by the same organization. Additionally, despite the name, agents are not explicitly required to use a terminal as their sole tool. Instead, they are free to manipulate the container however they please.
+    Terminus 2 is a simple scaffold created to serve as a neutral testbed for comparing model performance.
 
-    Many agents are installed directly into the container by a package manager and wield tools that are actually executable programs, far more complex than a typical Bash command. To account for the constraints and biases of most agents, and to remain true to the benchmark’s premise, a simple scaffold, Terminus 2, was created to serve as a neutral testbed for comparing model performance. Terminus 2 has a single tool, a headless terminal, and completes tasks using only Bash commands.
+    It has a single tool, a headless terminal.
+
+    It completes tasks using only Bash commands.
     """)
     return
 
@@ -397,9 +440,11 @@ def _(mo):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
-    #### Resuls
+    #### Selected Resuls
 
-    At time of publishing, Codex CLI paired with GPT-5.2 achieves the highest average resolution rate of 63%, followed by Terminus 2 with Claude Opus 4.5 and Terminus 2 with Gemini 3 Pro at 58% and 57%, respectively. Proprietary models paired with various agents occupy the top 13 positions in the rankings, with Terminus 2 and Kimi K2 Thinking performing best among the open-weight models, resolving 36% of tasks on average.
+    At time of publishing, Codex CLI paired with GPT-5.2 achieves the highest average resolution rate of 63%, followed by Terminus 2 with Claude Opus 4.5 and Terminus 2 with Gemini 3 Pro at 58% and 57%, respectively.
+
+    Proprietary models paired with various agents occupy the top 13 positions in the rankings, with Terminus 2 and Kimi K2 Thinking performing best among the open-weight models, resolving 36% of tasks on average.
 
     Running Terminal-Bench 2.0 costs anywhere from one to a hundred dollars, depending on the model’s price. Tasks usually take less than 20 minutes for most models, though a rare few take up to two hours with hundreds of API calls and almost 100 million tokens.
     """)
@@ -409,6 +454,46 @@ def _(mo):
 @app.cell
 def _(mo):
     mo.image('public/terminal-bench_difficulty.png')
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Upcoming Variants
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    #### Terminal-bench 3.0
+
+    There is an open call for contributions for [terminal-bench 3.0](https://www.tbench.ai/news/tb3-contribution-call).
+
+    The goal is to have 100 diverse tasks targeting at most 30% solve rate from the best models at time of release.
+
+    Broadly, they desire tasks that are longer-horizon, need richer environments, and require specialized expertise.
+
+    While terminal-bench 2.0 covers SWE, sys-admin, security, and scientific computing; terminal-bench 3.0 is expanding to a wider variety of domains, so long as they are realistic and accomplished via CLI.
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ### Terminal-bench-science
+
+    The goal of [terminal-bench-science](https://www.tbench.ai/news/tb-science-announcement) (TB-science) is to extend into complex real-world computational workflows that natural scientists run in their research labs.
+
+    The claim is that current "AI for science" benchmarks test textbook knowledge or abstrac capabilities like hypothesis generation, rather than measuring whether an AI system can execute the end-to-end computational workflows that drive modern research.
+
+    TB-science plans to port real workflows from leading research labs into executable benchmark tasks, evaluated in containerized environments with determenistic, programmatic verification.
+
+    There will be an open contribution call in Q2 2026.
+    """)
     return
 
 
@@ -544,40 +629,6 @@ def _():
 @app.cell
 def _():
     # tb_md(tb_by_difficulty[TB_HARD][0])
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    ### Upcoming Variants
-    """)
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    #### Terminal-bench 3.0
-
-    There is an open call for contributions for [terminal-bench 3.0](https://www.tbench.ai/news/tb3-contribution-call). The goal is to have 100 diverse tasks targeting at most 30% solve rate from the best models at time of release. Broadly they desire tasks that are longer-horizon, need richer environments, and require specialized expertise.
-
-    While terminal-bench 2.0 covers SWE, sys-admin, security, and scientific computing; terminal-bench 3.0 is expanding to a wider variety of domains, so long as they are realistic and accomplished via CLI.
-    """)
-    return
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    ### Terminal-bench-science
-
-    The goal of [terminal-bench-science](https://www.tbench.ai/news/tb-science-announcement) (TB-science) is to extend into complex real-world computational workflows that natural scientists run in their research labs.
-
-    The claim is that current "AI for science" benchmarks test textbook knowledge or abstrac capabilities like hypothesis generation, rather than measuring whether an AI system can execute the end-to-end computational workflows that drive modern research. TB-science plans to port real workflows from leading research labs into executable benchmark tasks, evaluated in containerized environments with determenistic, programmatic verification.
-
-    There will be an open contribution call in Q2 2026.
-    """)
     return
 
 
